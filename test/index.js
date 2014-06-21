67022697f28369e9b07b1b362ae932e7edd5246d
rjreed <rockyjreed@gmail.com>
2014-06-21 00:49:06 -0500

    Added support for files with no fm

--- test/index.js
+++ test/index.js
@@ -3,7 +3,27 @@ var fs = require('fs');
 var path = require('path');
 var runTests = require('./tests.js');
 
+var base = {
+  attributes: {
+    title: 'Facebook for Pugs',
+    date: 'June 18, 2014',
+    tags: ['facebook', 'pugs', 'cashmoney']
+  },
+  body: '# TODO: strategery'
+};
+
+var noFm = {
+	attributes: {},
+	body: 'What are snails even trying to do?'
+}
+
 //TODO: make better tests + better testing structure
-fs.readFile(path.resolve(__dirname, 'fixtures/', 'pugs.md'), { encoding: 'utf-8' }, function(err,data){
-    runTests(_(data));
+fs.readFile(path.resolve(__dirname, 'fixtures/', 'pugs.md'), 'utf-8', function(err,data){
+    runTests(_(data), base);
+    console.log('All base tests passed!');
+});
+
+fs.readFile(path.resolve(__dirname, 'fixtures/', 'nofm.md'), 'utf-8', function(err,data){
+    runTests(_(data), noFm);
+    console.log('All tests passed for non-frontmattered file!');
 });
--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,12 @@
 var assert = require('assert');
-var good = {
-  attributes: {
-    title: 'Facebook for Pugs',
-    date: 'June 18, 2014',
-    tags: ['facebook', 'pugs', 'cashmoney']
-  },
-  body: '# TODO: strategery'
-};
 
-module.exports = function(data) {
+
+module.exports = function(data, good) {
   assert.ok(data instanceof Object, 'Didn\'t return an object');
-  assert.deepEqual(Object.keys(data), Object.keys(good), 'Object  keys are incorrect');
-  assert.strictEqual(good.body, data.body, 'Body parsed incorrectly');
-  assert.deepEqual(good.attributes, data.attributes, 'Attributes parsed incorrectly');
-  console.log('All tests passed!');
+  assert.deepEqual(Object.keys(data), Object.keys(good), 
+    'Object.keys\nActual:\n' + Object.keys(data) + '\nExpected:\n' + Object.keys(good));
+  assert.strictEqual(good.body, data.body, 
+    'Body Parsing\nActual:\n' + data.body + '\nExpected:\n' + good.body);
+  assert.deepEqual(good.attributes, data.attributes, 
+    'Attributes Parsing\nActual:\n' + data.attributes + '\nExpected:\n' + good.attributes);
 };
