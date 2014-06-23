a6f039e5509b5bf80378834672a37d7902cecbc1
Rocky Reed <rockyjreed@gmail.com>
2014-06-23 16:04:33 -0500

    Fixed testing, added license, added package.json, added some dev depends for testing

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = function(data) {
-  var rx = /^---[\s]+([\s\S]*?)[\s]+---[\s]*([\s\S]*?)[\s]*$/;
+  var rx = /^---[\s]+([\s\S]*?)[\s]+---([\s\S]*?)$/;
   var rxFm = /(.*?)\s*:\s*(?:(?:\[\s*(.*?)(?=\s*\]))|(.*))/g;
   var rxArr = /\s?,\s?/g;
   var res = rx.exec(data) || [null, null, data];
   var tmp;
   var fm = {};
   fm.attributes = {};
-  fm.body = res[2];
+  fm.body = res[2].trim();
 
   while (tmp = rxFm.exec(res[1]), tmp) {
     fm.attributes[tmp[1]] = tmp[2] ? tmp[2].split(rxArr) : tmp[3];
--- test/index.js
+++ test/index.js
@@ -1,29 +1,56 @@
-var _ = require('../');
+var fm = require('../');
+var chai = require('chai');
+var should = chai.should();
 var fs = require('fs');
 var path = require('path');
-var runTests = require('./tests.js');
 
-var base = {
-  attributes: {
-    title: 'Facebook for Pugs',
-    date: 'June 18, 2014',
-    tags: ['facebook', 'pugs', 'cashmoney']
-  },
-  body: '# TODO: strategery'
-};
+function compareRes(opts) {
+  return describe(opts.label, function() {
+    var file, obj;
+    describe('Inital fs.readFile call', function() {
+      it('loads file contents', function(done) {
+        fs.readFile(path.resolve(__dirname, 'fixtures/', opts.filename), 'utf-8', function(err, data) {
+          file = data;
+          done();
+        });
+      });
+    });
+    describe('Tiny Front Matter', function() {
+      it('responds to function call and returns an object', function() {
+        obj = fm(file);
+        obj.should.exist.and.be.an('object');
+      });
+    });
+    describe('Returned Object', function() {
+      it('has the correct keys', function() {
+        obj.should.have.keys(['attributes', 'body']);
+      });
+      it('has the correct values', function() {
+        obj.attributes.should.eql(opts.ref.attributes);
+        obj.body.should.eql(opts.ref.body);
+      });
+    });
+  });
+}
 
-var noFm = {
-	attributes: {},
-	body: 'What are snails even trying to do?'
-};
-
-//TODO: make better tests + better testing structure
-fs.readFile(path.resolve(__dirname, 'fixtures/', 'pugs.md'), 'utf-8', function(err,data){
-    runTests(_(data), base);
-    console.log('All base tests passed!');
+compareRes({
+  label: 'Base Test',
+  filename: 'base.md',
+  ref: {
+    attributes: {
+      title: 'Facebook for Pugs',
+      date: 'June 18, 2014',
+      tags: ['facebook', 'pugs', 'cashmoney']
+    },
+    body: '# TODO: strategery'
+  }
 });
 
-fs.readFile(path.resolve(__dirname, 'fixtures/', 'nofm.md'), 'utf-8', function(err,data){
-    runTests(_(data), noFm);
-    console.log('All tests passed for non-frontmattered file!');
+compareRes({
+  label: 'No-metadata Test',
+  filename: 'nofm.md',
+  ref: {
+    attributes: {},
+    body: 'What are snails even trying to do?'
+  }
 });
